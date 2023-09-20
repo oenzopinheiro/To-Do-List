787d1505eaf074e2b312593dbfde32bb85b7719d
Enzo Pinheiro <oenzopinheiro@gmail.com>
2023-09-20 19:44:14 -0300

    change on css and html

--- main.js
+++ main.js
@@ -11,21 +11,31 @@ let openTasks = [];
 let inProgresTasks = [];
 let finishedTasks = [];
 
+function saveToLocalStorage() {
+    localStorage.setItem("openTasks", JSON.stringify(openTasks));
+    localStorage.setItem("inProgressTasks", JSON.stringify(inProgresTasks));
+    localStorage.setItem("finishedTasks", JSON.stringify(finishedTasks));
+}
+
 function createNewTask(listToAdd) { 
 
-    let element = {
-        id: "",
-        title: "",
-        description: ""
-    };
-
     if (listToAdd === 'open') {
+        let element = {
+            id: openTasks.length,
+            title: "",
+            description: ""
+        };
         openTasks.push(element);
         const id = openTasks.length - 1;
         openModal(id, listToAdd);
     }
 
     if (listToAdd === 'inProgress') {
+        let element = {
+            id: inProgresTasks.length,
+            title: "",
+            description: ""
+        };
         inProgresTasks.push(element);
         const id = inProgresTasks.length - 1;
         openModal(id, listToAdd);
@@ -40,35 +50,54 @@ function toggleModalSelect() {
 
 function changeStatusCard(id, listToRemove, listToAdd) {
 
+    console.log(id)
+    console.log(listToRemove)
+    console.log(listToAdd)
+
     let elementToAdd;
 
-    if(listToRemove === 'open') {
-        elementToAdd = openTasks.find(element  => element.id === id);
-        openTasks = openTasks.filter(element  => !( element.id === id));
+    if (listToRemove === 'open') {
+        elementToAdd = openTasks.find((element) => element.id === parseInt(id));
+        openTasks = openTasks.filter((element) => !(element.id === parseInt(id)));
+        console.log(openTasks)
     }
 
-    if(listToRemove === 'inProgress') {
-        elementToAdd =  inProgresTasks.find(element  => element.id === id);
-        inProgresTasks = inProgresTasks.filter(element  => !( element.id === id));
-    }
-    
-    if(listToRemove === 'finished') {
-        elementToAdd =  finishedTasks.find(element  => element.id === id);
-        finishedTasks = finishedTasks.filter(element  => !( element.id === id));
+    if (listToRemove === 'inProgress') {
+        elementToAdd = inProgresTasks.find((element) => element.id === parseInt(id));
+        inProgresTasks = inProgresTasks.filter((element) => !(element.id === parseInt(id)));
     }
 
-    if(listToAdd === 'open') {
-        openTasks.push(elementToAdd);
+    if (listToRemove === 'finished') {
+        elementToAdd = finishedTasks.find((element) => element.id === parseInt(id));
+        finishedTasks = finishedTasks.filter((element) => !(element.id === parseInt(id)));
     }
 
-    if(listToAdd === 'inProgress') {
-        inProgresTasks.push(elementToAdd);
+    if (listToAdd === 'open') {
+        if (elementToAdd) {
+            openTasks.push(elementToAdd);
+        }
     }
-    
-    if(listToAdd === 'finished') {
-        finishedTasks.push(elementToAdd);
-    } 
 
+    if (listToAdd === 'inProgress') {
+        console.log(elementToAdd)
+        if (elementToAdd) {
+            inProgresTasks.push(elementToAdd);
+            console.log(inProgresTasks)
+        }
+    }
+
+    if (listToAdd === 'finished') {
+        if (elementToAdd) {
+            finishedTasks.push(elementToAdd);
+        }
+    }
+
+    renderOpenList();
+    renderInProgressList();
+    renderFinishedList();
+
+    saveToLocalStorage();
+    closeModal();
 }
 
 function openModal(id, listToAdd) {
@@ -98,7 +127,7 @@ function openModal(id, listToAdd) {
             <button id="close-modal" onclick="updateTask(${id}
             , '${listToAdd}')">x</button>
         </div>
-        <div class="modal-select">
+        <div class="modal-select hide">
             <div class="select-option">
                 <span>Selecione uma opção</span>
             </div>
@@ -160,53 +189,43 @@ function closeModal() {
 
     renderOpenList();
     renderInProgressList();
-
+    renderFinishedList(); 
 }
 
 function updateTask(id, listToAdd) {
     const inputTitle = document.querySelector('#task-title')
     const inputDescription = document.querySelector('#task-description')
-
     if (listToAdd === 'open') {
         if(inputTitle.value  === "" && inputDescription.value === "" ) {
             openTasks.splice(id, 1);
             closeModal();
-            return;
+        } else {
+            openTasks[id].title = inputTitle.value
+            openTasks[id].description = inputDescription.value
         }
 
-        openTasks[id].title = inputTitle.value
-        openTasks[id].description = inputDescription.value
-        openTasks[id].id = id;
     }
 
     if (listToAdd === 'inProgress') {
         if(inputTitle.value  === "" && inputDescription.value === "" ) {
             inProgresTasks.splice(id, 1);
-            closeModal();
-            return;
+        } else {
+            inProgresTasks[id].title = inputTitle.value
+            inProgresTasks[id].description = inputDescription.value
         }
-
-        inProgresTasks[id].title = inputTitle.value
-        inProgresTasks[id].description = inputDescription.value
-        inProgresTasks[id].id = id;
     }
 
     if (listToAdd === 'finished') {
         if(inputTitle.value  === "" && inputDescription.value === "" ) {
+            console.log("teste")
             finishedTasks.splice(id, 1);
-            closeModal();
-            return;
+        } else {
+            finishedTasks[id].title = inputTitle.value
+            finishedTasks[id].description = inputDescription.value
         }
-
-        finishedTasks[id].title = inputTitle.value
-        finishedTasks[id].description = inputDescription.value
-        finishedTasks[id].id = id;
     }
 
-    localStorage.setItem("openTasks", JSON.stringify(openTasks));
-    localStorage.setItem("inProgressTasks", JSON.stringify(inProgresTasks));
-    localStorage.setItem("finishedTasks", JSON.stringify(finishedTasks));
-
+    saveToLocalStorage();
     closeModal();
 }
 
@@ -214,7 +233,6 @@ function init() {
     const openTasksData = localStorage.getItem("openTasks");
     const inProgressTasksData = localStorage.getItem("inProgressTasks");
     const finishedTasksData = localStorage.getItem("finishedTasks");
-
     if (openTasksData) {
         openTasks = JSON.parse(openTasksData);
     }
@@ -229,28 +247,27 @@ function init() {
 
     renderOpenList();
     renderInProgressList();
-
+    renderFinishedList(); 
 }
 
-function renderOpenList(id) {
+function renderOpenList() {
     let list = "";
 
-    openTasks.forEach((element) => {
-        list += `
-        <div class="card-task" onclick="openModal(${element.id}, 'open')">
-            <p class="card-description">${element.description}</p>
-            <div class="label-task red">
-                <img src="/Assets/img-title.png" class="img-title">
-                <h3 class="title">${element.title}</h3>
+    openTasks.forEach((element, id) => {
+            list += `
+            <div class="card-task" onclick="openModal(${id}, 'open')">
+                <p class="card-description">${element.description}</p>
+                <div class="label-task red">
+                    <img src="/Assets/img-title.png" class="img-title">
+                    <h3 class="title">${element.title}</h3>
+                </div>
             </div>
-        </div>
-        `;
+            `;
     });
 
     divListOne.innerHTML = list;
-
-    // implementar LocalStorage - Dica AQUI - DiCA Salvar Arrays
 }
+
 
 function renderInProgressList() {
     let list = "";
@@ -271,6 +288,25 @@ function renderInProgressList() {
     divListTwo.innerHTML = list;
 
 }
+
+function renderFinishedList() {
+    let list = "";
+
+    finishedTasks.forEach((element) => {
+        list += `
+        <div class="card-task" onclick="openModal(${element.id}, 'finished')">
+            <p class="card-description">${element.description}</p>
+            <div class="label-task green">
+                <img src="/Assets/img-title.png" class="img-title">
+                <h3 class="title">${element.title}</h3>
+            </div>
+        </div>
+        `;
+    });
+
+    divListThree.innerHTML = list;
+}
+
 
 
 //clica na tag do cabeçalho do modal vai chamar uma função que faz aparecer ou escondel o modal-select - dica: igual a fução de chamar o modal - add or remove class
